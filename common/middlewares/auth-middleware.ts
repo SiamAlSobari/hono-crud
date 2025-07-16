20f69eda55248d137703e817e6a786c29d113f37
YammD123 <siamqueen52@gmail.com>
2025-07-16 08:17:58 +0700

    feat(middleware): add auth-middleware

---
+++ common/middlewares/auth-middleware.ts
@@ -0,0 +1,21 @@
+import { Context, MiddlewareHandler } from 'hono'
+import { getCookie } from 'hono/cookie'
+import { verify } from 'hono/jwt'
+import { jwtSecret } from '../jwt/jwt-secret'
+
+
+export const authMiddleware : MiddlewareHandler = async (c:Context, next) => {
+    const token = getCookie(c, 'token')
+    // cek token
+    if (!token) return c.json({message: 'unauthenticated'}, 401)
+    
+    try {
+        // decode token
+        const payload = await verify(token, jwtSecret!)
+        //set user to context
+        c.set('user', payload)
+        await next()
+    } catch (error) {
+        return c.json({message: 'unauthenticated'}, 401)
+    }
+}
--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono";
 import { authController } from "../controllers/auth-controller";
 import { zValidator } from "@hono/zod-validator";
 import { authSchema } from "../schema/auth-schema";
+import { authMiddleware } from "../../common/middlewares/auth-middleware";
 
 export const authRoute = new Hono()
 
-authRoute.post('/register',zValidator('json',authSchema.register),authController.register)
+authRoute.post('/register',zValidator('json',authSchema.register),authMiddleware,authController.register)
