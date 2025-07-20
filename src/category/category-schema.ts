dc706d2cf89c9e93f4bf5f44e51f29b974746fa9
YammD123 <siamqueen52@gmail.com>
2025-07-20 20:46:59 +0700

    feat(category): add create and update category schema

---
+++ src/category/category-schema.ts
@@ -0,0 +1,23 @@
+import z from "zod";
+
+const createCategorySchema = z.object({
+  name: z
+    .string({ error: "Name wajib diisi" })
+    .min(3, "Name minimal 3 karakter")
+    .max(100),
+});
+
+const updateCategorySchema = z.object({
+  name: z
+    .string({ error: "Name wajib diisi" })
+    .min(3, "Name minimal 3 karakter")
+    .max(100),
+});
+
+export const categorySchema = {
+  createCategorySchema,
+  updateCategorySchema,
+};
+
+export type createCategoryInput = z.infer<typeof createCategorySchema>;
+export type updateCategoryInput = z.infer<typeof updateCategorySchema>;
